b319b22405353febaf23ebb21fb049c4f46871d8
preposterone-local-archlinux <ivan38rus@gmail.com>
2021-06-01 12:25:35 +0300

    Ex00 no loop refactor

--- src/ex00/Program.java
+++ src/ex00/Program.java
@@ -1,13 +1,21 @@
 
 public class Program {
 	public static void main(String[] args) {
-		int sumThis = -479598;
+		int sumThis = 123566;
 		int	result = 0;
 
-		for (int i = 0; i < 6; i++) {
-			result += sumThis % 10;
-			sumThis /= 10;
-		}
+		result += sumThis % 10;
+		sumThis /= 10;
+		result += sumThis % 10;
+		sumThis /= 10;
+		result += sumThis % 10;
+		sumThis /= 10;
+		result += sumThis % 10;
+		sumThis /= 10;
+		result += sumThis % 10;
+		sumThis /= 10;
+		result += sumThis % 10;
+
 		System.out.println(result);
 	}
 }
