a787bb8140c1aa9517a9c433ca4f404c4b3698f2
preposterone-on-travelmate <ivan38rus@gmail.com>
2021-06-01 09:23:37 +0300

    Ex04 rudimentary commit

---
+++ src/ex04/Program.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String input = scanner.nextLine();
+
+	}
+}
